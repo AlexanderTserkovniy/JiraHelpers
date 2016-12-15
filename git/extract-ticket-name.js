1facfe47f0a981c5082f4b4e9f5c1b75943c3f46
Alexander Tserkovniy <atserkovniy@smartling.com>
2016-12-15 11:58:13 +0200

    Tweaked extract ticket name.

--- git/extract-ticket-name.js
+++ git/extract-ticket-name.js
@@ -1,1 +1,1 @@
-javascript:($('#key-val').text() + '-' + $('#summary-val').text().toLowerCase().replace(/\W/g, '-'))
+javascript:+function () {var result = $('#key-val').text() + '-' + $('#summary-val').text().toLowerCase().replace(/\W/g, '-'); typeof copy !== 'undefined' ? copy(result) : console.log(result)}();
