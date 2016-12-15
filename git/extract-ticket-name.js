705cebe8fab1043345f1ebf6341cec78939f288d
Alexander Tserkovniy <atserkovniy@smartling.com>
2016-12-15 12:00:09 +0200

    alert() is better for copying extract ticket name.

--- git/extract-ticket-name.js
+++ git/extract-ticket-name.js
@@ -1,1 +1,1 @@
-javascript:+function () {var result = $('#key-val').text() + '-' + $('#summary-val').text().toLowerCase().replace(/\W/g, '-'); typeof copy !== 'undefined' ? copy(result) : console.log(result)}();
+javascript:+function () {var result = $('#key-val').text() + '-' + $('#summary-val').text().toLowerCase().replace(/\W/g, '-'); typeof copy !== 'undefined' ? copy(result) : alert(result)}();
